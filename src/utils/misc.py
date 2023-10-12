739164ce39530ae0d898656d1cc300ec9ecf8537
Soogeun Park <bwmelon97@naver.com>
2023-10-12 15:34:59 +0900

    feat: Modify consts.py, define RobotConfig.py
    - consts.py: reorganize constants by grouping them.
    - types.py: define RobotType class, which is an enum type of robots.
    - RobotConfig.py: define RobotConfig class, which contains robot-specific
      constants and functions.

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -6,32 +6,6 @@ Miscellaneous functions and variables.
 reachy_to_smpl_idx = [
     0, 2, 1, 5, 4, 9, 12, 17, 19, 21, 53, 40, 42, 16, 18, 20, 38, 25, 27, 24, 23,
 ]   # length: 21, smpl의 index: 23, smpl-x Body: 21
-# fmt: on
-
-joint_range = {
-    "r_shoulder_pitch": [-2.618, 1.57],
-    "r_shoulder_roll": [-3.14, 0.174],
-    "r_arm_yaw": [-1.57, 1.57],
-    "r_elbow_pitch": [-2.182, 0],
-    "r_forearm_yaw": [-1.745, 1.745],
-    "r_wrist_pitch": [-0.785, 0.785],
-    "r_wrist_roll": [-0.785, 0.785],
-    "l_shoulder_pitch": [-2.618, 1.57],
-    "l_shoulder_roll": [-0.174, 3.14],
-    "l_arm_yaw": [-1.57, 1.57],
-    "l_elbow_pitch": [-2.182, 0],
-    "l_forearm_yaw": [-1.745, 1.745],
-    "l_wrist_pitch": [-0.785, 0.785],
-    "l_wrist_roll": [-0.785, 0.785],
-    "neck_roll": [-0.4, 0.4],
-    "neck_pitch": [-0.4, 0.55],
-    "neck_yaw": [-1.4, 1.4],
-}
-"""
-Possible range of each joint's roll, pitch, yaw angles. -> maybe?
-
-joint list: [(shoulder, elbow, forearm, wrist for R, L), neck]
-"""
 
 reachy_to_smpl_idx = [0, 2, 1, 5, 4, 9, 12, 17, 19, 21, 53, 40, 42, 16, 18, 20, 38, 25, 27, 24, 23]
 
@@ -92,3 +66,4 @@ smplx_jname = [
     "left_wrist",
     "right_wrist",
 ]
+# fmt: on
---
+++ src/utils/types.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class RobotType(Enum):
+    """
+    Enum Type of Robots that we use
+    """
+
+    REACHY = "REACHY"
+    COMAN = "COMAN"
