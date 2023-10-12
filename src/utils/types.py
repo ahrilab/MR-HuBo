37c588beda17145f8c9d2b20eea47e2b0a0c20c9
Soogeun Park <soogeun@unist.ac.kr>
2023-10-12 16:07:15 +0900

    feat: generalize code -- data sampling
    - Use robot type & robot configuration class, instead of hard-coding.
    - Only changing the argument input of robot type will deal with the
      different robot types.

--- src/utils/types.py
+++ src/utils/types.py
@@ -1,3 +1,4 @@
+import argparse
 from enum import Enum
 
 
@@ -8,3 +9,33 @@ class RobotType(Enum):
 
     REACHY = "REACHY"
     COMAN = "COMAN"
+
+
+class SampleArgs(argparse.Namespace):
+    """
+    Arguments for Sampling Python Codes
+    """
+
+    num_seeds: int
+    motions_per_seed: int
+    robot_type: RobotType
+
+
+class Fit2SMPLArgs(argparse.Namespace):
+    """
+    Arguments for Converting Robot Joints for SMPL Parameters Python Codes
+    """
+
+    robot_type: RobotType
+    video_result_path: str
+    visualize: int
+    verbosity: int
+    fps: int
+
+
+class AdjustNeckArgs(argparse.Namespace):
+    """
+    Arguments for Adjust neck joints of robot Python Codes
+    """
+
+    robot_type: RobotType
